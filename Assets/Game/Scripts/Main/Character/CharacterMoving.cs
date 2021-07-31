e562804a5ad10c0e9b12eba5a1f3a22f322866cc
rStar <starlove124760@gmail.com>
2021-08-01 05:46:16 +0800

    auto Attack

--- Assets/Game/Scripts/Main/Character/CharacterMoving.cs
+++ Assets/Game/Scripts/Main/Character/CharacterMoving.cs
@@ -1,24 +1,39 @@
+using System;
 using System.Collections.Generic;
 using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
 using Zenject;
+using Random = UnityEngine.Random;
 
 public class CharacterMoving : MonoBehaviour
 {
+#region Private Variables
+
+    private bool         attack;
+    private bool         move;
+    private List<string> attackAnimations;
+
+    private string    ANIMATION_IDLE = "Idle";
+    private string    ANIMATION_MOVE = "Move";
+    private string    TAG_ENEMY      = "Enemy";
     private Transform trans;
+
+    [SerializeField]
+    private Animator animator;
 
     [SerializeField]
     private int moveSpeed = 3;
 
     [SerializeField]
-    private Animator animator;
+    private float AttackSpeed = 0.5f;
 
-    private string       ANIMATION_IDLE = "Idle";
-    private string       TAG_ENEMY      = "Enemy";
-    private string       ANIMATION_MOVE = "Move";
-    private bool         move;
-    private List<string> attackAnimations;
+    private int        attackCount;
+    private GameObject currentAttackingEnemy;
+
+#endregion
+
+#region Unity events
 
     // Start is called before the first frame update
     void Start()
@@ -32,11 +47,15 @@ public class CharacterMoving : MonoBehaviour
         animator.Play(ANIMATION_MOVE);
     }
 
-    private void OnTriggerEnemy(Collider2D obj)
+    // Update is called once per frame
+    void Update()
     {
-        move = false;
-        animator.Play(GetAttackAnimationName());
+        if (move) Move();
     }
+
+#endregion
+
+#region Private Methods
 
     private string GetAttackAnimationName()
     {
@@ -44,14 +63,36 @@ public class CharacterMoving : MonoBehaviour
         return attackAnimations[randomValue];
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (move) Move();
-    }
-
     private void Move()
     {
         trans.Translate(trans.right * moveSpeed * Time.deltaTime);
     }
+
+    private void OnTriggerEnemy(Collider2D obj)
+    {
+        currentAttackingEnemy = obj.gameObject;
+        move                  = false;
+        attack                = true;
+        animator.Play(ANIMATION_IDLE);
+        var attackTimeSpan = TimeSpan.FromSeconds(AttackSpeed);
+        Observable.Interval(attackTimeSpan , Scheduler.MainThread)
+                  .TakeWhile(l => attack)
+                  .Subscribe(PlayAttackAnimation);
+    }
+
+    private void PlayAttackAnimation(long obj)
+    {
+        attackCount++;
+        animator.Play(GetAttackAnimationName());
+        if (attackCount == 3)
+        {
+            attack      = false;
+            move        = true;
+            attackCount = 0;
+            currentAttackingEnemy.SetActive(false);
+            animator.Play(ANIMATION_MOVE);
+        }
+    }
+
+#endregion
 }
